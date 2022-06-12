408b2e3c22321b84d2ad12999024bbe5cd71085b
Ksant <mkonstn@gmail.com>
2022-06-12 13:10:44 +0300

    Test binding

---
+++ TangyWeb_Models/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Tangy_Models.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Active { get; set; }
+        public double Price { get; set; }
+    }
+}
--- Tangy_WebAssembly/Program.cs
+++ Tangy_WebAssembly/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Tangy_WebAssembly;
+using TangyWeb_Assembly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
